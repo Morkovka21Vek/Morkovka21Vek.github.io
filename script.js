6518bcc73846bd6c9f060db62a92368ce45623d7
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-07-03 22:39:11 +0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('themeToggle').addEventListener('click', function() {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+        document.leftList.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
