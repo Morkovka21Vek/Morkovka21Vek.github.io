8ef451dedecd0fc07db95d47ba5ff4b4469eb613
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-07-19 17:17:01 +0300

    update

---
+++ ru/main.js
@@ -0,0 +1,279 @@
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/any_hash_number/g, function () {
+      return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+  };
+
+  let result = '???> ';
+  let text = [
+    'python',
+    'Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32<br>Type "help", "copyright", "credits" or "license" for more information.',
+    'from users import Morkovka21Vek',
+    'print(Morkovka21Vek.hello)',
+    "Привет, я <u class='consoleLinkMorkovka21Vek'>Morkovka21Vek</u>",
+    'print(Morkovka21Vek.data)',
+];
+
+new_result = '???> python<br>Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32<br>Type "help", "copyright", "credits" or "license" for more information.<br>>>>from users import Morkovka21Vek<br>print(Morkovka21Vek.hello)<br>'+"Привет, я <u class='consoleLinkMorkovka21Vek'>Morkovka21Vek</u><br>"+'print(Morkovka21Vek.data)<br>{<br>"Платформа": "Windows 10"<br>"Страна": "Россия/Москва"<br>"время": "UTC +03:00"<br>}<br>';
+
+is_typing = false;
+mode = "python";
+
+text_new_result = [];
+prefix_new_result = [];
+
+// new_result = '???> python<br>Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32<br>Type "help", "copyright", "credits" or "license" for more information.<br>>>>from users import Morkovka21Vek<br>print(Morkovka21Vek.hello)<br>'+"Hi, I'm <u class='consoleLinkMorkovka21Vek'>Morkovka21Vek</u><br>"+'print(Morkovka21Vek.data)<br>{<br>"platform": "Windows 10"<br>"country": "Russia/Moskow"<br>"time": "UTC +03:00"<br>}<br>'
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let progectTrigger = document.querySelector('.MoveConsoleTrigger');
+    let Console = document.querySelector('.console');
+    let bottomConsol = document.querySelector('.bottomConsol');
+ 
+    function checkTriggerProgectsVisibility() {
+        let windowHeight = window.innerHeight;
+ 
+        /* position: fixed; */
+        let progectTriggerPosition = progectTrigger.getBoundingClientRect().top;
+        if (progectTriggerPosition < windowHeight - 100) {
+            // bottomConsol.style.display = "block"
+            bottomConsol.style.visibility = "visible"
+            bottomConsol.style.transform = "translateY(0)";
+            bottomConsol.style.opacity = "1";
+            Console.style.opacity = "0";
+
+            // bottomConsol.style.position = "fixed";
+            // bottomConsol.style.top = "auto";
+            bottomConsol.style.bottom = "0";
+            // Console.style.transform = "translate(-50%, "+(2000 - bottomConsolPosition)+"px)";
+            // Console.style.bottom = "0";
+            if (mode == "python") {
+                prefix_new_result.push(">>>")
+                text_new_result.push("quit()");
+                if (is_typing == false) {
+                    is_typing = true;
+                    typeLineProgects();
+                }
+                mode = "cmd"
+            }
+        } else {
+            // bottomConsol.style.display = "none"
+            bottomConsol.style.transform = "translateY(-150%)";
+            bottomConsol.style.opacity = "0";
+            Console.style.opacity = "1";
+            bottomConsol.style.visibility = "hidden"
+        }
+    }
+ 
+    checkTriggerProgectsVisibility();
+
+    let blocks = document.querySelectorAll('.progects-block');
+ 
+    function checkBlocksVisibility() {
+        let windowHeight = window.innerHeight;
+ 
+        blocks.forEach(block => {
+            let blockPosition = block.getBoundingClientRect().top;
+            if (block.id.indexOf('opened') == -1){
+                if (blockPosition < windowHeight - 350) {
+                    text = []
+                    block.style.opacity = "1";
+                    block.style.transform = "translateY(0)";
+                    // typeLineProgects("img.show(imgs/"+block.id+")");
+                    prefix_new_result.push("???>")
+                    text_new_result.push("git clone https://github.com/Morkovka21Vek/"+block.id+".git");
+                    // console.log(text_new_result);
+                    if (is_typing == false) {
+                        is_typing = true;
+                        typeLineProgects();
+                    }
+                    block.id = block.id+"-opened";
+                }
+            }
+        });
+    }
+ 
+    checkBlocksVisibility();
+
+    function checkDemoVisibility() {
+        let block = document.querySelector('.selection-online-demos');
+        let windowHeight = window.innerHeight;
+        if (block.id.indexOf('opened') == -1){
+            let blockPosition = block.getBoundingClientRect().top;
+            if (blockPosition < windowHeight - 350) {
+                block.style.opacity = "1";
+                // block.style.transform = "translateY(0)";
+                prefix_new_result.push("???>")
+                text_new_result.push("git clone https://github.com/Morkovka21Vek/morkovka21vek.github.io.git");
+                if (is_typing == false) {
+                    is_typing = true;
+                    typeLineProgects();
+                }
+                block.id = block.id+"-opened";
+            }
+        }
+    }
+ 
+    checkDemoVisibility();
+
+    window.addEventListener('scroll', function() {
+        checkBlocksVisibility();
+        checkTriggerProgectsVisibility();
+        checkDemoVisibility();
+    });
+
+    // addLink = (url) => ``
+    let imgs = document.querySelectorAll('.progects-img');
+    var date = new Date();
+    imgs.forEach(img => {
+        var Img_Link = img.src.format(String(date.getFullYear())+date.getMonth()+date.getDate()+date.getHours())
+        // alert(String(date.getFullYear())+date.getMonth()+date.getDate()+date.getHours()+date.getMinutes())
+        // alert(Img_Link)
+        // const Img_Link = "https://opengraph.githubassets.com/"+date.getFullYear()+date.getMonth()+date.getDate()+date.getHours()+date.getMinutes()+"/Morkovka21Vek/AI_Font_Generator"
+        img.setAttribute("src", Img_Link);
+    })
+
+    let lastCommitsH = document.querySelectorAll('.progects-last-commit');
+    lastCommitsH.forEach(myH => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", `https://api.github.com/repos/Morkovka21Vek/${myH.id}/branches/main`);
+        xhr.send();
+        // img.setAttribute("src", Img_Link);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                var json = JSON.parse(xhr.response);
+                var commitDate = String(json.commit.commit.committer.date);
+                console.log(commitDate);
+                var splitCommitDate = commitDate.split('-');
+                var rightCommitDate = String(splitCommitDate[2]).split('T')[0] +'.'+ String(splitCommitDate[1]) +'.'+ splitCommitDate[0];
+                var LastCommitDateOut = "Последнее изменение: "+rightCommitDate;
+                console.log(LastCommitDateOut);
+                // myH.setAttribute("innerHTML", LastCommitDateOut);
+                myH.innerHTML = LastCommitDateOut;
+        }
+          };
+          
+        xhr.onerror = function() {
+        console.log('Connection error');
+        };
+    })
+    
+    // <div class='consoleLinkMorkovka21VekDiv' onmouseover='changeItem()' onmouseout='rechangeItem()'></div>
+    // <div id='consoleLinkMorkovka21VekDiv' onmouseover='changeItem()' onmouseout='rechangeItem()'></div>
+    let line = 0;
+    let count = 0;
+    // let result = 'Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32<br>Type "help", "copyright", "credits" or "license" for more information.<br>>>> ';
+    function typeLine() {
+        if (text == []){
+            return true
+        }
+        let interval = setTimeout(
+        () => {
+        if (text.length == 0) return true
+        result += text[line][count];
+        // alert(document.getElementById('pre123'))
+        document.querySelectorAll('.consoleTextP').forEach(consolePtext => {
+            consolePtext.innerHTML = result;// + '|';
+            // consolePtext.scrollTop = consolePtext.scrollHeight;
+        })
+        document.querySelectorAll('.consoleText').forEach(consoletext => {
+            consoletext.scrollTop = consoletext.scrollHeight;
+        })
+        count++;
+        if (count >= text[line].length) {
+            count = 0;
+            result += "<br>";
+            line++;
+        
+        
+        if (line == 1 || line == 4) {
+            result += text[line];
+            result += "<br>";
+            line++;
+        }
+
+        if (line == 2 || line == 3 || line == 5) {
+            result += ">>> ";
+        }
+
+        if (line == 6) {
+            setTimeout(() => {
+                result += '{<br>"Платформа": "Windows 10"<br>"Страна": "Россия/Москва"<br>"Время": "UTC +03:00"<br>}'
+                document.querySelectorAll('.consoleTextP').forEach(consolePtext => {
+                    consolePtext.innerHTML = result;// + '|';
+                    // consolePtext.scrollTop = consolePtext.scrollHeight;
+                })
+                document.querySelectorAll('.consoleText').forEach(consoletext => {
+                    consoletext.scrollTop = consoletext.scrollHeight;
+                })
+                return true;
+            }, 500);
+        }
+
+        if (line == text.length) {
+            clearTimeout(interval);
+            // document.getElementById('consoleTextP').innerHTML = result;
+            return true;
+        }
+        }
+        typeLine();
+            }, 25)
+        }
+        setTimeout(() => { typeLine(); }, 3500);
+
+    let typeLineProgectsCount = 0;
+    let my_new_string = null;
+    function typeLineProgects() {
+        let interval = setTimeout(
+        () => {
+        if (my_new_string == null) {
+            my_new_string = text_new_result.shift();
+            new_result += prefix_new_result.shift();
+        }
+        new_result += my_new_string[typeLineProgectsCount];
+        document.querySelectorAll('.consoleTextP').forEach(consolePtext => {
+            consolePtext.innerHTML = new_result;
+        })
+        document.querySelectorAll('.consoleText').forEach(consoletext => {
+            consoletext.scrollTop = consoletext.scrollHeight;
+        })
+        typeLineProgectsCount++;
+        if (typeLineProgectsCount >= my_new_string.length) {
+            typeLineProgectsCount = 0;
+            new_result += "<br>";
+            document.querySelectorAll('.consoleTextP').forEach(consolePtext => {
+                consolePtext.innerHTML = new_result;
+            })
+            document.querySelectorAll('.consoleText').forEach(consoletext => {
+                consoletext.scrollTop = consoletext.scrollHeight;
+            })
+            if (text_new_result.length == 0) {
+                clearTimeout(interval);
+                is_typing = false;
+                my_new_string = null;
+                return true;
+            } else {
+                my_new_string = text_new_result.shift();
+                new_result += prefix_new_result.shift();
+            }
+        }
+        typeLineProgects();
+            }, 25)
+        }
+});
+
+// function changeItem() {
+//     document.getElementById('consoleLinkMorkovka21VekDiv').style.display = "block";
+//     // alert("hello")
+// }
+// function rechangeItem() {
+//     document.getElementById('consoleLinkMorkovka21VekDiv').style.display = "none";
+// }
+
+// const AI_Font_Generator_Img = document.getElementById('AI_Font_Generator_Img');
+// var date = new Date();
+// alert(AI_Font_Generator_Img_Link)
+// const AI_Font_Generator_Img_Link = "https://opengraph.githubassets.com/"+date.getFullYear()+date.getMonth()+date.getDate()+date.getHours()+date.getMinutes()+"/Morkovka21Vek/AI_Font_Generator"
+// // alert(strLink)
+// AI_Font_Generator_Img.setAttribute("src", AI_Font_Generator_Img_Link);
