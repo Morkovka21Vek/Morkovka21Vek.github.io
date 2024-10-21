6aed7d1b27f21a9a89b3f7c1b6c54d98f4a59330
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-10-21 22:06:40 +0300

    Pretty code

--- forSchool/BreadMarket/main.js
+++ forSchool/BreadMarket/main.js
@@ -1,59 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let container = document.querySelector('.container');
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://gist.githubusercontent.com/Morkovka21Vek/e08485da0c9de11892ec5b9e2a7016bf/raw/2fffde5b7402950e08c882e3c1e0e2565dfd7132/testForSchoolBreadShop.json");
-    xhr.send();
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            var json = JSON.parse(xhr.response);
-            console.log(json.categories);
-            json.categories.forEach(category => {
+document.addEventListener("DOMContentLoaded", function () {
+  let container = document.querySelector(".container");
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://gist.githubusercontent.com/Morkovka21Vek/e08485da0c9de11892ec5b9e2a7016bf/raw/2fffde5b7402950e08c882e3c1e0e2565dfd7132/testForSchoolBreadShop.json"
+  );
+  xhr.send();
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      var json = JSON.parse(xhr.response);
+      console.log(json.categories);
+      json.categories.forEach((category) => {
+        let categoryDiv = document.createElement("div");
+        categoryDiv.className = "category";
+        categoryDiv.id = category.id;
 
-                let categoryDiv = document.createElement("div");
-                categoryDiv.className = "category";
-                categoryDiv.id = category.id;
+        let categoryName = document.createElement("div");
+        categoryName.className = "categoryName";
+        categoryName.innerText = category.name;
+        categoryDiv.append(categoryName);
 
-                let categoryName = document.createElement("div");
-                categoryName.className = "categoryName";
-                categoryName.innerText = category.name;
-                categoryDiv.append(categoryName);
+        category.products.forEach((product) => {
+          let productDiv = document.createElement("div");
+          productDiv.className = "product";
+          productDiv.id = product.id;
 
-                category.products.forEach(product => {
-                    let productDiv = document.createElement("div");
-                    productDiv.className = "product";
-                    productDiv.id = product.id;
+          let productText = document.createElement("div");
+          productText.className = "text";
 
-                    let productText = document.createElement("div");
-                    productText.className = "text";
+          if (product.price !== "") product.price = "<br>" + product.price;
+          if (product.yellowText !== "")
+            product.yellowText =
+              '<br><p class="yellowText">' + product.yellowText + "</p>";
+          if (product.redText !== "")
+            product.redText =
+              '<br><p class="redText">' + product.redText + "</p>";
+          productText.innerHTML = `${product.text}${product.price}${product.yellowText}${product.redText}`;
+          productDiv.append(productText);
 
-                    if (product.price !== "") product.price = "<br>" + product.price;
-                    if (product.yellowText !== "") product.yellowText = '<br><p class="yellowText">' + product.yellowText + "</p>";
-                    if (product.redText !== "") product.redText = '<br><p class="redText">' + product.redText + "</p>";
-                    productText.innerHTML = `${product.text}${product.price}${product.yellowText}${product.redText}`;
-                    productDiv.append(productText);
+          product.imgs.forEach((imgSrc) => {
+            let productImg = document.createElement("img");
+            productImg.src = imgSrc;
+            productDiv.append(productImg);
+          });
 
-                    product.imgs.forEach(imgSrc => {
-                        let productImg = document.createElement("img");
-                        productImg.src = imgSrc;
-                        productDiv.append(productImg);
-                    });
+          categoryDiv.append(productDiv);
+        });
 
-                    categoryDiv.append(productDiv);
-                });
-
-                container.append(categoryDiv);
-            });
-            // var commitDate = String(json.commit.commit.committer.date);
-            // console.log(commitDate);
-            // var splitCommitDate = commitDate.split('-');
-            // var rightCommitDate = String(splitCommitDate[2]).split('T')[0] +'.'+ String(splitCommitDate[1]) +'.'+ splitCommitDate[0];
-            // var LastCommitDateOut = myH.id + rightCommitDate;
-            // console.log(LastCommitDateOut);
-            // myH.innerHTML = LastCommitDateOut;
+        container.append(categoryDiv);
+      });
+      // var commitDate = String(json.commit.commit.committer.date);
+      // console.log(commitDate);
+      // var splitCommitDate = commitDate.split('-');
+      // var rightCommitDate = String(splitCommitDate[2]).split('T')[0] +'.'+ String(splitCommitDate[1]) +'.'+ splitCommitDate[0];
+      // var LastCommitDateOut = myH.id + rightCommitDate;
+      // console.log(LastCommitDateOut);
+      // myH.innerHTML = LastCommitDateOut;
     }
-      };
-      
-    xhr.onerror = function() {
-        console.error('Connection error');
-    };
+  };
+
+  xhr.onerror = function () {
+    console.error("Connection error");
+  };
 });
--- forSchool/BreadMarket/shoppingCart.js
+++ forSchool/BreadMarket/shoppingCart.js
@@ -1,7 +1,6 @@
-  document.querySelectorAll('.addToShoppingCart').forEach(button => {
-    button.addEventListener('click', function() {
-      const productName = button.parentNode.parentNode.id;
-      alert(`${productName} добавлено в корзину!`);
-    });
+document.querySelectorAll(".addToShoppingCart").forEach((button) => {
+  button.addEventListener("click", function () {
+    const productName = button.parentNode.parentNode.id;
+    alert(`${productName} добавлено в корзину!`);
   });
-
+});
