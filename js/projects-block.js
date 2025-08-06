b5d432436276f1ff5744fceef880afc5e1f4f73f
Morkovka21Vek <morkovka21vek@gmail.com>
2025-08-06 04:24:03 +0300

    feat: add action update

--- js/projects-block.js
+++ js/projects-block.js
@@ -31,29 +31,3 @@ adjustLayoutForScreenSize();
 
 // Добавляем обработчик события resize
 window.addEventListener("resize", adjustLayoutForScreenSize);
-
-const imgs = document.querySelectorAll(".projects-img");
-const date = new Date();
-imgs.forEach((img) => {
-  const imgLink = `https://opengraph.githubassets.com/${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}/Morkovka21Vek/${img.parentElement.id}`;
-  img.setAttribute("src", imgLink);
-});
-
-document.querySelectorAll(".projects-last-commit").forEach((commitElement) => {
-  const repoId = commitElement.parentElement.parentElement.id;
-
-  fetch(`https://api.github.com/repos/Morkovka21Vek/${repoId}/branches/main`)
-    .then((response) => {
-      if (!response.ok) throw new Error("Failed to fetch commit data");
-      return response.json();
-    })
-    .then((data) => {
-      const commitDate = new Date(data.commit.commit.committer.date);
-      const formattedDate = commitDate.toLocaleDateString("ru-RU");
-      commitElement.textContent = `Last Commit: ${formattedDate}`;
-    })
-    .catch((error) => {
-      console.error("Error fetching commit data:", error);
-      commitElement.textContent = "Error fetching commit data";
-    });
-});
